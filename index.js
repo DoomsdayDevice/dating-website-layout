ffeeeff7a3f31d59684059724518a9419229ffaf
dima <lovesjuicypeaches@gmail.com>
2019-05-04 15:28:31 +0600

    initial commit after breaking the project apart

---
+++ index.js
@@ -0,0 +1,34 @@
+let startButton = document.querySelector(".start-button");
+startButton.addEventListener("click", scrollToJoin);
+let joinButton = document.querySelector(".join-circle-button");
+joinButton.addEventListener("click", scrollToJoin);
+
+function scrollToJoin(){
+    let locationOfJoin = 500;
+    // let quote = document.querySelector(".quote");
+    let signup = document.querySelector(".sign-up");
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    let signupX = signup.getBoundingClientRect().top + scrollTop - 280;
+    console.log("quote X:", signupX);
+    window.scroll(0, signupX);
+}
+
+let burger = document.querySelector(".line-container");
+let menuLine = document.querySelector(".burger");
+let dropdown = document.querySelector(".dropdown");
+menuLine.addEventListener("click", menuButtonPress);
+
+
+let menuIsOpen = false;
+function menuButtonPress(){
+    if (!menuIsOpen){
+        burger.style.transform = "rotate(-90deg)";
+        menuIsOpen = true;
+        dropdown.style.opacity = "1";
+    } else {
+        burger.style.transform = "";
+        menuIsOpen = false;
+        dropdown.style.opacity = "0";
+    }
+}
