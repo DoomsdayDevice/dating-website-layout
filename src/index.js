2edaab88e09be3444e21d8c4c38c508689a9fe03
dima <lovesjuicypeaches@gmail.com>
2019-06-24 22:09:18 +0600

    adjusted image responsiveness

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
+// add jump events to both buttons
 const startButton = document.querySelector(".top__start-btn .btn");
-startButton.addEventListener("click", scrollToJoin);
+startButton.addEventListener("click", jumpToJoin);
 const joinButton = document.querySelector(".bottom__join-circle-btn");
-joinButton.addEventListener("click", scrollToJoin);
+joinButton.addEventListener("click", jumpToJoin);
 
-function scrollToJoin(){
-    const locationOfJoin = 500;
-    // let quote = document.querySelector(".quote");
-    const signup = document.querySelector(".mid__sign-up");
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+function jumpToJoin(){
+  const locationOfJoin = 500;
+  const signup = document.querySelector(".mid__sign-up");
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    let signupX = signup.getBoundingClientRect().top + scrollTop - 280;
-    window.scroll(0, signupX);
+  let signupX = signup.getBoundingClientRect().top + scrollTop - 280;
+  window.scroll(0, signupX);
 }
 
-const burger = document.querySelector(".burger__bars");
-const menuLine = document.querySelector(".menu__burger");
-const dropdown = document.querySelector(".menu__dropdown");
-menuLine.addEventListener("click", handleMenuButtonPress);
 
-
+// add hamburger menu open/close event
 let menuIsOpen = false;
 function handleMenuButtonPress(){
-    if (!menuIsOpen){
-        burger.style.transform = "rotate(-90deg)";
-        menuIsOpen = true;
-        dropdown.style.opacity = "1";
-    } else {
-        burger.style.transform = "";
-        menuIsOpen = false;
-        dropdown.style.opacity = "0";
-    }
+  const burger = document.querySelector(".burger__bars");
+  const dropdown = document.querySelector(".menu__dropdown");
+  if (!menuIsOpen){
+    burger.style.transform = "rotate(-90deg)";
+    menuIsOpen = true;
+    dropdown.style.opacity = "1";
+  } else {
+    burger.style.transform = "";
+    menuIsOpen = false;
+    dropdown.style.opacity = "0";
+  }
 }
-
-let scrollTop;
-const pic = document.querySelector('.top__main-pic');
-const point = 760;
+document.querySelector(".menu__burger").addEventListener("click", handleMenuButtonPress);
